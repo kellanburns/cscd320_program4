cb62ff46f805ac31bfb81bfda47eca172e19b4c3
kellanburns <129882265+kellanburns@users.noreply.github.com>
2024-11-23 20:02:25 -0800

    file reader finished

--- src/TopoSort.java
+++ src/TopoSort.java
@@ -1,5 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TopoSort {
-    public static class Node {
+    private static class Node {
         public int value;
         public Node next;
 
@@ -9,7 +13,7 @@ public class TopoSort {
         }
     }
 
-    public static class LinkedList {
+    private static class LinkedList {
         public Node head;
 
         public void add(int value) {
@@ -26,11 +30,46 @@ public class TopoSort {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 1){
             throw new IllegalArgumentException("Not enough args");
         }
 
+        String inputFileName = args[0];
+        LinkedList[] G = fileReader(inputFileName);
 
+
+    }
+
+    private static LinkedList[] fileReader(String inputFileName) throws IOException {
+        LinkedList[] G = new LinkedList[lineCounter(inputFileName)];
+        String line;
+
+        try(BufferedReader inputFile = new BufferedReader(new FileReader(inputFileName))) {
+            while ((line = inputFile.readLine()) != null) {
+                String[] parts = line.split(":");
+                int vertex = Integer.parseInt(parts[0].trim());
+                G[vertex] = new LinkedList();
+                if (parts.length > 1) {
+                    String[] neighbors = parts[1].split(",");
+                    for (String n: neighbors)
+                        G[vertex].add(Integer.parseInt(n.trim()));
+                }
+            }
+        }
+
+        return G;
+    }
+
+    public static int lineCounter(String inputFileName) throws IOException {
+        int counter = 0;
+
+        try(BufferedReader inputFile = new BufferedReader(new FileReader(inputFileName))) {
+            while (inputFile.readLine() != null) {
+                counter++;
+            }
+        }
+
+        return counter;
     }
 }
